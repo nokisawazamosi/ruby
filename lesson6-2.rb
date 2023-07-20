55e66a77cda4ef3355ec3e818c4c9aef255d5b8c
EC2 Default User <ec2-user@ip-172-31-20-101.ap-northeast-1.compute.internal>
2023-07-20 11:26:08 +0000

    [Finish]アルゴリズムを学ぼう5章

--- lesson6-2.rb
+++ lesson6-2.rb
@@ -5,14 +5,14 @@ i=1
 while i<=input do
   a=gets.to_i
   b=gets.to_i
-  
+
   puts "a=#{a}"
   puts "b=#{b}"
-  
+
   puts "#{a}+#{b}=#{a+b}"
   puts "#{a}-#{b}=#{a-b}"
   puts "#{a}*#{b}=#{a*b}"
   puts "#{a}/#{b}=#{a/b}"
-  
+
   i+=1
 end
